0f2cba2ea57967327db2cb7e7b42c8c2a2b6e5b2
Thomas Hucorne <thomas.hucorne@ensiie.eu>
2025-12-01 10:07:56 +0100

    First submission with XGBoost

---
+++ submissions/xgboost_classifier/classifier.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.pipeline import make_pipeline
+
+
+def preprocess_log(X):
+    """
+    Application du log(1 + x) pour réduire l'écart entre les valeurs
+    """
+    X_log = np.log1p(X)
+    return X_log
+
+class Classifier(object):
+    def __init__(self):
+        # Use scikit-learn's pipeline
+        self.pipe = make_pipeline(
+            StandardScaler(with_mean=False, with_std=True),
+            PCA(n_components=50),
+            HistGradientBoostingClassifier(
+                max_depth=2, max_features=1.0
+            ),
+        )
+
+    def fit(self, X_sparse, y):
+        X = preprocess_log(X_sparse)
+        self.pipe.fit(X, y)
+
+        pass
+
+    def predict_proba(self, X_sparse):
+        X = preprocess_log(X_sparse)
+        # here we use HistGradientBoosting.predict_proba()
+        return self.pipe.predict_proba(X)
